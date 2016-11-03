5dc68f7e31503f8f3d1f6c03ad4a6f8ac7f5606d
Jack WIlde <jackwilde630@gmail.com>
2016-11-03 01:27:41 -0400

    refactored shops component into multiple files

--- assets/app/shops.component.ts
+++ assets/app/shops.component.ts
@@ -1,87 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
 import {Shop}      from './shop';
 import {ShopService} from './shop.service';
 
 
 @Component({
+    moduleId: module.id,
     selector: 'my-shops',
-    template: `
-              <h2>SHOPS!</h2>
-              <ul class="shops">
-                <li *ngFor="let shop of shops"
-                [class.selected]="shop === selectedShop"
-                (click)="onSelect(shop)">
-                  <span class="badge">{{shop.id}}</span> {{shop.name}}
-                </li>
-              </ul>
-              <my-shop-detail [shop] = "selectedShop"></my-shop-detail>
-              `
-    ,
-    styles: [`
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .shops {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 15em;
-      }
-      .shops li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .shops li.selected:hover {
-        background-color: #BBD8DC !important;
-        color: white;
-      }
-      .shops li:hover {
-        color: #607D8B;
-        background-color: #DDD;
-        left: .1em;
-      }
-      .shops .text {
-        position: relative;
-        top: -3px;
-      }
-      .shops .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0 0 4px;
-      }
-    `]
-
+    templateUrl: 'shops.component.html',
+    styleUrls: ['shops.component.css']
 })
-export class ShopsComponent {
+
+export class ShopsComponent implements OnInit {
   title = 'Shop Local';
-  constructor(shopService: ShopService) {
-    shopService.getShops()
+  selectedShop: Shop
+
+  constructor(
+    private shopService: ShopService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.shopService.getShops()
         .subscribe(
         shops => this.shops = shops,
         error => console.error('Error:' + err),
         () => console.log('Completed!')
       );
   }
-  // shops = SHOPS;
-  selectedShop: Shop
 
   onSelect(shop: Shop): void {
     this.selectedShop = shop;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedShop.id])
   }
 }
